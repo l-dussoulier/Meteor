d2b5b0832c70f570a23c852024b910198ce0c1e9
Louis <dussoulier.louis@hotmai.fr>
2021-01-29 15:52:47 +0100

    Add response layout

--- client/main.js
+++ client/main.js
@@ -5,13 +5,26 @@ import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
+  var ct = this;
   this.counter = new ReactiveVar(0);
+  this.test = new ReactiveVar("Default");
+  HTTP.call("GET","http://localhost:3000/ws",{}, function (err,res){
+    if (err){
+      console.log(err);
+    }else{
+      console.log(res.content);
+      ct.test.set(res.content);
+    }
+  });
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  test(){
+    return Template.instance().test.get();
+  }
 });
 
 Template.hello.events({
--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+    console.log("coucou");
 });
 
 WebApp.connectHandlers.use('/ws', (req, res, next) => {
