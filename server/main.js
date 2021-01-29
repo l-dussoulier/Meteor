0ef6cb199184748513212a9cd14ad254095994c4
marienfarge <marien.farge@gmail.com>
2021-01-29 14:46:03 +0100

    premiere reponse serveur

--- server/main.js
+++ server/main.js
@@ -3,3 +3,8 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+WebApp.connectHandlers.use('/ws', (req, res, next) => {
+    res.writeHead(200);
+    res.end('Enchanté je suis le serveur');
+});
