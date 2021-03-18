160b3bc7c15e52f2cfb71350a4828167a93e21fd
antoine2116 <antooinerobin@gmail.com>
2021-03-18 10:30:22 +0100

    Ajout Mongo Db (pas fini)

--- client/main.js
+++ client/main.js
@@ -4,47 +4,57 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
   var ct = this;
-  this.counter = new ReactiveVar(0);
-  this.test = new ReactiveVar("Default");
   this.movies = new ReactiveVar([]);
-  // call fichier json
-/*  HTTP.call("GET","http://localhost:3000/movies",{}, function (err,res){
-    if (err){
-      console.log(err);
-    }else{
-      ct.movies.set(JSON.parse(res.content).results);
-      console.log(JSON.parse(res.content).results);
-    }
-  });*/
 
-// partie api web
+  // partie api web
   HTTP.call("GET","http://localhost:3000/api/movies",{}, function (err,res){
     if (err){
       console.log(err);
-    }else{
+    } else{
       ct.movies.set(JSON.parse(res.content).results);
-      //console.log(JSON.parse(res.content).results);
     }
   });
 });
 
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-  test(){
-    return Template.instance().test.get();
-  },
   movies() {
     return Template.instance().movies.get();
   }
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  'click .like':function(event){
+    updateVote(event.currentTarget, true);
+  }
 });
+
+Template.hello.events({
+  'click .dislike':function(event) {
+    updateVote(event.currentTarget, false);
+  }
+});
+
+function updateVote(element, like) {
+  console.log(element.dataset.votecount)
+  HTTP.call("POST", "http://localhost:3000/api/updateVote",
+      {
+        headers: {
+          "content-type":"application/json",
+          "Accept":"application/json"
+        },
+        data: {
+          'id': parseInt(element.dataset.id),
+          'like': like,
+          'vote_count': parseInt(element.dataset.votecount)
+        }
+      },
+      function (err,res) {
+        if (err) {
+          console.log(err);
+        } else {
+          element.closest('.card-footer').getElementsByClassName('count-vote')[0].innerHTML = res.content;
+        }
+  });
+}
+
--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 import { localDatas} from "./local-datas";
+
+const films = new Mongo.Collection('films');
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -11,24 +14,57 @@ WebApp.connectHandlers.use('/ws', (req, res, next) => {
     res.end('Enchanté je suis le serveur');
 });
 
-
 WebApp.connectHandlers.use('/movies', (req, res, next) => {
-    res.writeHead(200);
-    res.end(
-        JSON.stringify(localDatas)
-    );
+    if (err) {
+        console.log(err);
+        res.writeHead(500);
+    } else {
+        res.end(JSON.stringify(localDatas));
+        res.writeHead(200);
+    }
 });
 
 WebApp.connectHandlers.use('/api/movies', (req, res, next) => {
-    res.writeHead(200);
+    JSON.stringify(HTTP.call("GET","https://api.themoviedb.org/3/discover/movie?api_key=1793c4843a64fbd6fdba88ce08e45c5f&language=fr-FR",{},function (err,response){
+        if (err) {
+            console.log(err);
+            res.writeHead(500);
+        } else {
+            res.end(JSON.stringify(response.data));
+            res.writeHead(200);
+        }
+    }));
+});
 
-        JSON.stringify(HTTP.call("GET","https://api.themoviedb.org/3/discover/movie?api_key=1793c4843a64fbd6fdba88ce08e45c5f&language=fr-FR",{},function (err,response){
-            if (err){
-                console.log(err);
-            }else{
-                res.end(
-                    JSON.stringify(response.data)
-                );
-            }
-        }));
+WebApp.connectHandlers.use('/api/updateVote', (req, res, next) => {
+    var body = "";
+    req.on('data', Meteor.bindEnvironment(function (data) {
+        body += data;
+    }));
+
+    req.on('end', Meteor.bindEnvironment(function () {
+        let data = JSON.parse(body);
+        console.log(data);
+        let id = data.id
+        let like = data.like
+        let dataVoteCount = data.vote_count;
+
+        let newVoteCount;
+        let film = films.find({id:id});
+
+        if (film.count() == 0) {
+            newVoteCount = like ? dataVoteCount + 1 : dataVoteCount - 1;
+            films.insert({
+                id: id,
+                vote_count: newVoteCount
+            });
+        } else {
+            let currentVoteCount = film.fetch()[0].vote_count;
+            newVoteCount = like ? currentVoteCount + 1 : currentVoteCount - 1;
+            films.update({id : id},  { $set: {vote_count: newVoteCount} })
+        }
+
+        res.writeHead(200);
+        res.end(JSON.stringify(newVoteCount));
+    }));
 });
